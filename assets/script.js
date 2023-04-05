defe2e4af1b7fd1c8a496fb444230a2ef1644fb4
Océane <vallaya02@gmail.com>
2023-04-05 16:10:32 +0200

    Modification du slide au clic sur flèches

--- assets/script.js
+++ assets/script.js
@@ -18,10 +18,61 @@ const slides = [
 ]
 const flecheGauche = document.querySelector (".arrow_left");
 const flecheDroite = document.querySelector (".arrow_right");
+let dots = document.querySelectorAll(".dot");
+let dotsIndex = 0;
+let imgSlides = document.querySelectorAll(".banner-img");
+let bannerP = document.querySelector("#banner p")
+imgSlides.forEach(img => img.style.display = "none");
+imgSlides[0].style.display = "block";
+const changeSlides = (dotIndex) => {
+	dots.forEach(dot => dot.classList.remove("dot_selected"));
+	dots[dotIndex].classList.add("dot_selected");
+	imgSlides.forEach(img => img.style.display = "none");
+	imgSlides[dotIndex].style.display = "block";
+	bannerP.innerHTML = slides[dotIndex].tagLine;
+}
 
 flecheGauche.addEventListener("click", function() {
 	console.log ("fleche gauche cliquee");
+	if (dotsIndex === 0) {
+		dotsIndex = 3
+	} else {
+		dotsIndex -= 1
+	}
+	switch (dotsIndex) {
+		case 0:
+			changeSlides(0)
+			break;
+		case 1:
+			changeSlides(1)
+			break;
+		case 2:
+			changeSlides(2)
+			break;
+		case 3:
+			changeSlides(3)
+			break;	
+	}
 })
 flecheDroite.addEventListener("click", function() {
 	console.log ("fleche droite cliquee");
+	if (dotsIndex === 3) {
+		dotsIndex = 0
+	} else {
+		dotsIndex += 1 
+	}
+	switch (dotsIndex) {
+		case 0:
+			changeSlides(0)
+			break;
+		case 1:
+			changeSlides(1)
+			break;
+		case 2:
+			changeSlides(2)
+			break;
+		case 3:
+			changeSlides(3)
+			break;		
+	}
 })
